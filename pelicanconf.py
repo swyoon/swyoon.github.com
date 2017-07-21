69db7099a486fe782b724c4e8bb24efc196ac607
swyoon <sangwoong24yoon@gmail.com>
2017-07-22 02:39:27 +0900

    theme applied

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Sangwoong Yoon'
 SITENAME = "swyoon's blog"
-SITEURL = 'swyoon.github.io'
+SITEURL = 'http://swyoon.github.io'
 
 PATH = 'content'
 
